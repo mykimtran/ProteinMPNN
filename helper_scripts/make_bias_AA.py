456a18f9b00dbe55709f84e774dcfb47a30f8582
My Kim Tran <mykim_tran@yahoo.de>
2025-06-16 23:10:55 -0700

    black -l 120 . (format everything)

--- helper_scripts/make_bias_AA.py
+++ helper_scripts/make_bias_AA.py
@@ -1,4 +1,5 @@
-import argparse 
+import argparse
+
 
 def main(args):
 
@@ -10,18 +11,18 @@ def main(args):
 
     my_dict = dict(zip(AA_list, bias_list))
 
-    with open(args.output_path, 'w') as f:
-        f.write(json.dumps(my_dict) + '\n')
+    with open(args.output_path, "w") as f:
+        f.write(json.dumps(my_dict) + "\n")
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     argparser.add_argument("--output_path", type=str, help="Path to the output dictionary")
-    argparser.add_argument("--AA_list", type=str, default='', help="List of AAs to be biased")
-    argparser.add_argument("--bias_list", type=str, default='', help="AA bias strengths")
+    argparser.add_argument("--AA_list", type=str, default="", help="List of AAs to be biased")
+    argparser.add_argument("--bias_list", type=str, default="", help="AA bias strengths")
 
     args = argparser.parse_args()
     main(args)
 
-#e.g. output
-#{"A": -0.01, "G": 0.02}
+# e.g. output
+# {"A": -0.01, "G": 0.02}
